762f48db4396f6eaa5e724ab5affe6b2c0fb7092
Ram <ramachandrannellai@gmail.com>
2024-11-13 19:55:32 +0100

    feat: javadoc added for Position and Point

---
+++ src/main/java/com/github/nramc/geojson/domain/Point.java
@@ -0,0 +1,121 @@
+package com.github.nramc.geojson.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.github.nramc.geojson.constant.GeoJsonType;
+import com.github.nramc.geojson.validator.GeoJsonValidationException;
+import com.github.nramc.geojson.validator.ValidationError;
+import com.github.nramc.geojson.validator.ValidationResult;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents a GeoJSON Point geometry.
+ * <p>
+ * A GeoJSON Point is defined by a single {@link Position} which includes the coordinates
+ * (longitude, latitude, and optional altitude) of the point. This class provides methods
+ * to create and validate Point objects in compliance with the GeoJSON specification.
+ * </p>
+ *
+ * <p>Note: The constructor does not perform validation eagerly. To validate a {@code Point}
+ * object, the {@link #validate()}  or {@link #isValid()} method must be called explicitly.
+ * Alternatively, you can use the static factory methods that validate the data eagerly
+ * and throw {@code GeoJsonValidationException} if the data is invalid.
+ * </p>
+ *
+ * <p>Example usage:
+ * <pre>{@code
+ * Point point = Point.of(40.7128, -74.0060);
+ * }</pre></p>
+ */
+public final class Point extends Geometry {
+    private final String type;
+    private final Position coordinates;
+
+    /**
+     * Constructs a new {@code Point} with the specified type and coordinates.
+     * <p>If object created using constructor directly, then validation does not performed eagerly.
+     * To perform validation, use below options,
+     * <ol>
+     *     <li>{@link Point#validate()} to perform validation and return result</li>
+     *     <li>{@link Point#isValid()} to check whether GeoJson valid or not.</li>
+     * </ol>
+     * </p>
+     *
+     * @param type        The type of the GeoJSON object, which must be "Point".
+     * @param coordinates The {@link Position} representing the coordinates of the point.
+     */
+    @JsonCreator
+    public Point(String type, Position coordinates) {
+        this.type = type;
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * Creates a new {@code Point} instance with the specified coordinates, performing validation.
+     *
+     * @param coordinates The {@link Position} representing the coordinates of the point.
+     * @return A validated {@code Point} object.
+     * @throws GeoJsonValidationException if the provided coordinates are invalid.
+     */
+    public static Point of(Position coordinates) {
+        return validateAndThrowErrorIfInvalid(new Point(GeoJsonType.POINT, coordinates));
+    }
+
+    /**
+     * Creates a new {@code Point} instance from longitude and latitude values, performing validation.
+     *
+     * @param longitude The longitude of the point.
+     * @param latitude  The latitude of the point.
+     * @return A validated {@code Point} object.
+     * @throws GeoJsonValidationException if the provided longitude or latitude are invalid.
+     */
+    public static Point of(long longitude, long latitude) {
+        return validateAndThrowErrorIfInvalid(new Point(GeoJsonType.POINT, Position.of(longitude, latitude)));
+    }
+
+    /**
+     * Creates a new {@code Point} instance from longitude, latitude, and altitude values, performing validation.
+     *
+     * @param longitude The longitude of the point.
+     * @param latitude  The latitude of the point.
+     * @param altitude  The altitude of the point.
+     * @return A validated {@code Point} object.
+     * @throws GeoJsonValidationException if the provided values are invalid.
+     */
+    public static Point of(long longitude, long latitude, long altitude) {
+        return validateAndThrowErrorIfInvalid(new Point(GeoJsonType.POINT, Position.of(longitude, latitude, altitude)));
+    }
+
+    /**
+     * Validates the {@code Point} object, checking for any errors in the type or coordinates.
+     *
+     * @return A {@link ValidationResult} object containing any validation errors found.
+     */
+    @Override
+    public ValidationResult validate() {
+        Set<ValidationError> errors = new HashSet<>();
+        if (StringUtils.isBlank(type) || !StringUtils.equals(type, GeoJsonType.POINT)) {
+            errors.add(ValidationError.of("type", "type '%s' is not valid. expected '%s'".formatted(type, GeoJsonType.POINT), "type.invalid"));
+        }
+        if (coordinates == null) {
+            errors.add(ValidationError.of("coordinates", "coordinates should not be empty/blank", "coordinates.invalid.empty"));
+        } else {
+            ValidationResult coordinateValidationResult = coordinates.validate();
+            if (coordinateValidationResult.hasErrors()) {
+                errors.addAll(coordinateValidationResult.getErrors());
+            }
+        }
+
+        return new ValidationResult(errors);
+    }
+
+    private static Point validateAndThrowErrorIfInvalid(Point point) {
+        ValidationResult validationResult = point.validate();
+        if (validationResult.hasErrors()) {
+            throw new GeoJsonValidationException("GeoJson Invalid", validationResult.getErrors());
+        }
+        return point;
+    }
+}
--- src/main/java/com/github/nramc/geojson/domain/Position.java
+++ src/main/java/com/github/nramc/geojson/domain/Position.java
@@ -5,49 +5,131 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.github.nramc.geojson.validator.GeoJsonValidationException;
 import com.github.nramc.geojson.validator.Validatable;
+import com.github.nramc.geojson.validator.ValidationError;
 import com.github.nramc.geojson.validator.ValidationResult;
 
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Represents a GeoJSON Point, defined by a single geographical position.
+ * <p>
+ * A Point in GeoJSON consists of a single coordinate pair (latitude, longitude)
+ * and optionally an altitude. This class provides methods to access these
+ * coordinates and ensures they adhere to the valid ranges defined in the GeoJSON
+ * specification (RFC 7946).
+ * </p>
+ *
+ * <ol>
+ *     <li>The longitude of the point, ranging from -180 to 180 degrees.</li>
+ *     <li>The latitude of the point, ranging from -90 to 90 degrees.</li>
+ *     <li>The altitude of the point, there is no strict range validation, but usually ranging from -11000 to 11000 meters.</li>
+ * </ol>>
+ *
+ * <p>Example: {@code new Point(37.7749, -122.4194)}</p>
+ */
 public class Position implements Validatable, Serializable {
 
     @JsonValue
     private final double[] coordinates;
 
+    /**
+     * Constructs a Point with empty latitude, longitude and altitude.
+     * Mainly useful for An Object-Relational Mapping(ORM) tools which does not support parameterized constructor to persist object into repository.
+     */
+    public Position() {
+        this.coordinates = new double[2];
+    }
+
+    /**
+     * Constructs a Point with the given latitude, longitude and optional altitude.
+     * Does not perform validation eagerly.
+     *
+     * <p>If object created using constructor directly, then use below option for validation,</p>
+     * <ol>
+     *     <li>{@link Position#validate()} to perform validation and return result</li>
+     *     <li>{@link Position#isValid()} to check whether GeoJson valid or not.</li>
+     * </ol>
+     *
+     * @param coordinates [longitude, latitude, altitude]
+     */
     @JsonCreator
     public Position(double[] coordinates) {
         this.coordinates = coordinates;
     }
 
+    /**
+     * Validate the given latitude, longitude and optional altitude, Constructs a Point only when given coordinates valid.
+     * Otherwise, throws {@link GeoJsonValidationException} with validation errors.
+     *
+     * @param coordinates [longitude, latitude, altitude]
+     * @throws GeoJsonValidationException with validation errors
+     */
     public static Position of(double[] coordinates) {
         return validateAndThrowErrorIfInvalid(new Position(coordinates));
     }
 
+    /**
+     * Validate the given latitude and longitude, Constructs a Point only when given coordinates valid.
+     * Otherwise, throws {@link GeoJsonValidationException} with validation errors.
+     *
+     * @param longitude The longitude of the point
+     * @param latitude  The latitude of the point
+     * @throws GeoJsonValidationException with validation errors
+     */
     public static Position of(double longitude, double latitude) {
         return validateAndThrowErrorIfInvalid(new Position(new double[]{longitude, latitude}));
     }
 
+    /**
+     * Validate the given latitude, longitude and altitude, Constructs a Point only when given coordinates valid.
+     * Otherwise, throws {@link GeoJsonValidationException} with validation errors.
+     *
+     * @param longitude The longitude of the point
+     * @param latitude  The latitude of the point
+     * @param altitude  The altitude of the point
+     * @throws GeoJsonValidationException with validation errors
+     */
     public static Position of(double longitude, double latitude, double altitude) {
         return validateAndThrowErrorIfInvalid(new Position(new double[]{longitude, latitude, altitude}));
     }
 
+    /**
+     * Returns the coordinates of the point, contains latitude, longitude and optional altitude.
+     *
+     * @return The [longitude, latitude, altitude]
+     */
     @JsonIgnore
     public double[] getCoordinates() {
         return this.coordinates;
     }
 
+    /**
+     * Returns the longitude of the point.
+     *
+     * @return The longitude, ranging from -180 to 180 degrees.
+     */
     @JsonIgnore
     public double getLongitude() {
         return coordinates.length > 0 ? coordinates[0] : Double.NaN;
     }
 
+    /**
+     * Returns the latitude of the point.
+     *
+     * @return The latitude, ranging from -90 to 90 degrees.
+     */
     @JsonIgnore
     public double getLatitude() {
         return coordinates.length > 1 ? coordinates[1] : Double.NaN;
     }
 
+    /**
+     * Returns the altitude of the point.
+     *
+     * @return The altitude, usually ranging from -11000 to 11000 meters, but no guaranty for the range.
+     */
     @JsonIgnore
     public double getAltitude() {
         return coordinates.length > 2 ? coordinates[2] : Double.NaN;
@@ -68,21 +150,32 @@ public class Position implements Validatable, Serializable {
         return latitude >= -90 && latitude <= 90;
     }
 
-    @JsonIgnore
-    @Override
-    public boolean isValid() {
-        return !validate().hasErrors();
-    }
-
+    /**
+     * Validates the coordinates of the current GeoJSON object and checks if they conform to
+     * the expected ranges and lengths. The method performs several checks:
+     * <ul>
+     *     <li>Ensures that the length of the coordinates array is either 2(longitude & latitude) or 3(longitude, latitude & altitude).</li>
+     *     <li>Validates the longitude value to ensure it is within the acceptable range between -180 and 180, both inclusive.</li>
+     *     <li>Validates the latitude value to ensure it is within the acceptable range between -90 and 90, both inclusive.</li>
+     * </ul>
+     *
+     * <p>If any of these validations fail, the corresponding error keys are added to a set of
+     * errors, which is then wrapped in a {@code ValidationResult} object and returned.</p>
+     *
+     * @return A {@code ValidationResult} object containing any validation errors found. If there
+     * are no errors, the returned {@code ValidationResult} will contain an empty set of errors {@code ValidationError}.
+     * @see ValidationResult
+     * @see ValidationError
+     */
     @Override
     public ValidationResult validate() {
-        Set<String> errors = new HashSet<>();
+        Set<ValidationError> errors = new HashSet<>();
         if (!isLengthValid(getCoordinates())) {
-            errors.add("coordinates.length.invalid");
+            errors.add(ValidationError.of("coordinates", "coordinates length is not valid", "coordinates.length.invalid"));
         } else if (!isLongitudeValid(getLongitude())) {
-            errors.add("coordinates.longitude.invalid");
+            errors.add(ValidationError.of("longitude", "longitude is not valid", "coordinates.longitude.invalid"));
         } else if (!isLatitudeValid(getLatitude())) {
-            errors.add("coordinates.latitude.invalid");
+            errors.add(ValidationError.of("latitude", "latitude is not valid", "coordinates.latitude.invalid"));
         }
         return new ValidationResult(errors);
     }
