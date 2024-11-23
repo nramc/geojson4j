cc8c94509933d008b618400feb4d51ed06ab4745
Ram <ramachandrannellai@gmail.com>
2024-11-23 21:13:05 +0100

    feat(ci): job added for testing example modules

--- examples/spring-mvc-example/src/main/java/com/github/nramc/geojson/examples/repository/GeoJsonDataRepository.java
+++ examples/spring-mvc-example/src/main/java/com/github/nramc/geojson/examples/repository/GeoJsonDataRepository.java
@@ -16,6 +16,7 @@
 package com.github.nramc.geojson.examples.repository;
 
 import com.github.nramc.geojson.examples.repository.entity.GeoJsonDataEntity;
+import org.springframework.data.repository.ListCrudRepository;
 
 public interface GeoJsonDataRepository extends ListCrudRepository<GeoJsonDataEntity, Long> {
 }
